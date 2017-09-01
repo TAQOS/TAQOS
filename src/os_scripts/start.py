98122d5caa9b4bf8d97f651280ce99483741c501
harleypatton <hpatton@berkeley.edu>
2017-09-01 16:04:23 -0700

    fix SpOoKy merge conflict

--- src/os_scripts/start.py
+++ src/os_scripts/start.py
@@ -6,9 +6,6 @@
 #   Created on: July 13, 2017
 #   Last modified: July 13, 2017
 #
-<<<<<<< HEAD
-###############################################################################
-=======
 ###############################################################################
 
 import sys
@@ -17,4 +14,3 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from taqos.runner.shell import start_repl
 
 start_repl()
->>>>>>> 1d6e199dc5cca7974cbf9a518123bb510ea565a7
